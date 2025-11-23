c6197958782384b41148828efbaf3033666d936a
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-11-23 17:50:33 +0530

    College Overloading

--- Learning/CollegeOverloading.cpp
+++ Learning/CollegeOverloading.cpp
@@ -13,11 +13,20 @@ public:
     void operator+(const A& o) { 
         cout << "Sum is: (" << x + o.x << ", " << y + o.y << ")" << endl;
     }
+
+    bool operator==(const A& o) {
+        return x == o.x && y == o.y;
+    }
+
 };
 
 int main() {
     A a1(10, 45);
     A a2(20, 46);
     a1 + a2;
+    A a3(10, 45);
+    cout << "Are a1 and a2 equal? " << (a1 == a2 ? "Yes" : "No") << endl;
+    cout << "Are a1 and a3 equal? " << (a1 == a3 ? "Yes" : "No") << endl;
+    cout << "Are a1, a2 and a3 equal? " << (a1 == a3 && a1 == a2 ? "Yes" : "No") << endl;
     return 0;
 }
